db26d8657a9f22f097a8ec8849408c864d153502
jhonatanmizu <natanjesuss20@gmail.com>
2022-08-03 20:42:17 -0300

    Created project basic structure

---
+++ blog/app.js
@@ -0,0 +1,25 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 3000;
+// DATABASE
+const { ConnectDB, sequelize } = require('./database/db')
+ConnectDB().then(() => {
+    console.log("CONECTADO AO DB");
+}).catch((err) => {
+    console.log("Não foi possível se conectar ao db");
+})
+// BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// VIEW ENGINE
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// ROUTES
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`);
+});
---
+++ blog/database/db.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("guiapress", "root", "password", {
+    host: "localhost",
+    dialect: "mariadb",
+});
+
+async function ConnectDB() {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+module.exports = { sequelize, ConnectDB }
