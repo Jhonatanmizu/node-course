107ea794c072a7d7063c3994b9ae7f4ed7cf43ba
jhonatanmizu <natanjesuss20@gmail.com>
2022-08-06 23:16:41 -0300

    finished consumer with axios

---
+++ consumerAxios/services/api.js
@@ -0,0 +1,57 @@
+const api = axios.create({
+    baseURL: 'http://localhost:3000/',
+    timeout: 1000,
+    headers: {
+
+        "Content-type": 'application/json'
+    }
+});
+
+
+api.get('games').then(resp => {
+    console.log(resp.data);
+    resp.data.forEach(game => {
+        createGame(game)
+    });
+}).catch(err => {
+    console.error(err)
+})
+
+function createGame(game) {
+    const li = document.createElement('li')
+    li.innerText = game.title
+    li.setAttribute('id', game.id)
+    deleteGame()
+    const ul = document.querySelector('#games')
+    ul.appendChild(li)
+}
+
+function saveGame(event, form) {
+    event.preventDefault()
+    console.log(form.children[0].value);
+    let gameModel = {
+        // id: Math.floor(Math.random() * 100),
+        title: form.children[0].value,
+        year: form.children[1].value,
+        price: form.children[2].value,
+    }
+    console.log(gameModel);
+    api.post('game', gameModel).then(resp => {
+        console.log(resp);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+function deleteGame() {
+    const items = document.querySelectorAll('#games li')
+    items.forEach(i => {
+        i.addEventListener('click', () => {
+            api.delete(`game/${i.id}`).then(resp => {
+                console.log(resp);
+            }).catch(err => {
+                console.error(err)
+            })
+        })
+    })
+    console.log('clicked', items);
+}
